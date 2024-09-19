28100f0b46d6bcdf92d1a2feb4b1e3c90b91ed17
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-19 15:51:16 +0200

    [UPD] added 'Category' and 'Food' instances

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
         public $category; // istanza della classe Category che devo passare come argomento
 
         // utilizzo il costruttore per inizializzare le proprietà
-        public function __construct($name, $image, $price, $is_available = true, Category $category) {
+        public function __construct($name, $image, $price, $is_available, Category $category) {
             $this->name = $name;
             $this->image = $image;
             $this->price = $price;
-            $this->is_avaible = $is_available;
+            $this->is_available = $is_available;
             $this->category = $category;
         }
 
         // definisco un metodo per restituire una stringa che contiene i valori assegnati
         public function getProductInfo() {
-            return 'Name'.$this->name.',Price'.$this->price;
+            return 'Name:'.' '.$this->name.', Price:'.' '.$this->price;
         }
 
     }
@@ -38,7 +38,7 @@
     class Food extends Product {
         protected $calories;
 
-        public function __construct($name, $image, $price, $is_available = true, Category $category) {
+        public function __construct($name, $image, $price, $is_available, Category $category) {
 
             // richiamo il costruttore della classe genitore per inizializzare le proprietà ereditate
             parent::__construct($name, $image, $price, $is_available, $category);
@@ -55,6 +55,19 @@
         }
 
     }
+
+    // instanze della classe Category
+    $dog = new Category('Dogs');
+    $cat = new Category('Cats');
+
+    // istanza della classe Food e associo la categoria
+    $food = new Food('Crochette', 'immagine', '99.99', true, $dog);
+
+    // richiamo il setter setCalories e gli assegno un valore
+    $food->setCalories(9000);
+
+    var_dump($food);
+    var_dump($food->getProductInfo());
 
 ?>
 
