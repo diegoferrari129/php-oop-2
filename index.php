1a65e7fc672a28ffc36d20ded708983b9134002d
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-19 14:49:42 +0200

    [UPD] added comments

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
+    // definisco la classe base Product
     class Product {
+        // assegno delle proprietà alla classe Product
         public $name;
         public $image;
         public $price;
         public $is_available;
-        public $category;
+        public $category; // istanza della classe Category che devo passare come argomento
 
+        // utilizzo il costruttore per inizializzare le proprietà
         public function __construct($name, $image, $price, $is_available = true, Category $category) {
             $this->name = $name;
             $this->image = $image;
@@ -15,12 +18,14 @@
             $this->category = $category;
         }
 
+        // definisco un metodo per restituire una stringa che contiene i valori assegnati
         public function getProductInfo() {
             return 'Name'.$this->name.',Price'.$this->price;
         }
 
     }
 
+    // definisco la classe Category che viene inglobata dalla classe Product
     class Category {
         public $name;
 
@@ -29,17 +34,22 @@
         }
     }
 
+    // definisco la sottoclasse Food per aggiungiere delle proprietà alla classe Product
     class Food extends Product {
         protected $calories;
 
         public function __construct($name, $image, $price, $is_available = true, Category $category) {
+
+            // richiamo il costruttore della classe genitore per inizializzare le proprietà ereditate
             parent::__construct($name, $image, $price, $is_available, $category);
         }
 
+        // definisco un metodo setter per impostare il valore della proprietà $calories
         public function setCalories($calories){
             $this->calories = $calories;
         }
 
+        // definisco un metodo getter per restituire il valore definito dal metodo setter
         public function getCalories() {
             return $this->calories;
         }
